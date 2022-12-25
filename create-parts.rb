16541deeeeb75605ab681a19cffb49cde1e959da
Jeff Wong <awole20@gmail.com>
2022-12-25 03:17:04 -0800

    maybe this is working??
    updating magick call to generate and place the 4 images in the correct folders
    on the next levels.

--- create-parts.rb
+++ create-parts.rb
@@ -5,6 +5,11 @@ ROOT_FOLDER = "#{__dir__}/parts/"
 START_LEVEL = 14
 # starting zoom is 14. up to 22. So 8 passes.
 # Then join with black spaces to generate down to 1. 13 passes.
+# split filename - get current zoom level, x and y
+# next 4 tiles is: current zoom ++
+# new-x = x^2-1, x^2
+# new-y = y^2-1, y^2
+# original size: 1920x1920 -- crop and split from here. resize *up* to 256.
 def make_next_tileset(tile)
   parts = tile.gsub(ROOT_FOLDER, "").split("/")
   puts parts
@@ -17,16 +22,25 @@ def make_next_tileset(tile)
   FileUtils.mkdir_p parts_folder_1
   FileUtils.mkdir_p parts_folder_2
 
+  `magick "#{tile}" -crop "2x2@" "#{File.join(ROOT_FOLDER, "temp_%d.png")}"`
+  # 0 upper left
+  # 1 upper right
+  # 2 lower left
+  # 3 lower right
+  temp_file_1 = File.join(ROOT_FOLDER, "temp_0.png")
+  temp_file_2 = File.join(ROOT_FOLDER, "temp_2.png")
+  temp_file_3 = File.join(ROOT_FOLDER, "temp_1.png")
+  temp_file_4 = File.join(ROOT_FOLDER, "temp_3.png")
+
   new_file_1 = File.join(parts_folder_1,  "#{(y**2) - 1}.png")
   new_file_2 = File.join(parts_folder_1,  "#{(y**2)}.png")
   new_file_3 = File.join(parts_folder_2,  "#{(y**2) - 1}.png")
   new_file_4 = File.join(parts_folder_2,  "#{(y**2)}.png")
 
-  # split filename - get current zoom level, x and y
-  # next 4 tiles is: current zoom ++
-  # new-x = x^2-1, x^2
-  # new-y = y^2-1, y^2
-  # original size: 1920x1920 -- crop and split from here. resize *up* to 256.
+  FileUtils.mv(temp_file_1, new_file_1)
+  FileUtils.mv(temp_file_2, new_file_2)
+  FileUtils.mv(temp_file_3, new_file_3)
+  FileUtils.mv(temp_file_4, new_file_4)
 end
 
 def make_next_zoom(tile_folder)
@@ -38,14 +52,14 @@ def make_next_zoom(tile_folder)
 end
 
 # 256x256px final sizes
-# resize down to 256 in the final passes.
-# TODO: Resize all 22 levels down to 256px
+# resize to 256 in the final passes.
+# TODO: Resize all 22 levels to 256px
 # Resizes a set of files in a folder
 def final_resize_pass(tile_folder)
   Dir.foreach(tile_folder) do |filename|
     next unless filename =~ /png$/
     file = tile_folder + filename
-    `magick "#{file}" -resize "256x256\>" "#{file}"`
+    `magick "#{file}" -resize "256x256" "#{file}"`
   end
 end
 
